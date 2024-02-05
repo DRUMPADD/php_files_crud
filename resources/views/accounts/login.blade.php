e9bc0409399eb88a2bbe7b4438739aef0201ff2b
Erick Patiño <erickfco1999@gmail.com>
2024-02-04 20:15:25 -0600

    Login form changed

--- resources/views/accounts/login.blade.php
+++ resources/views/accounts/login.blade.php
@@ -5,40 +5,43 @@
 @endsection
 @section('content')
     <h1>Fluidos Mcgreen de México</h1>
+    @include('layouts.partials.messages')
     <div class="form-box">
         <abbr title="Registro o inicio de sesión">
         <p class="change-option">
             <i class="fa-solid fa-right-to-bracket"></i>
         </p>
         </abbr>
-        <form class="form-login active">
-        <h3>Iniciar sesión</h3>
-        <div class="form-content">
-            <label for="user">Usuario</label>
-            <div class="form-input">
-                <input type="text" name="usuario" class="input" id="user">
-                <p>
-                    <i class="fa-regular fa-user"></i>
-                </p>
-            </div>
-        </div>
-        <div class="form-content">
-            <label for="password">Contraseña</label>
-            <div class="form-input">
-                    <input type="password" name="password" class="input" id="password">
-                    <p class="hide-password hide">
-                    <i class="fa-regular fa-eye"></i>
+        <form action="/login_user" method="post" class="form-login active">
+            <h3>Iniciar sesión</h3>
+            @csrf
+            <div class="form-content">
+                <label for="user">Usuario</label>
+                <div class="form-input">
+                    <input type="text" name="email" class="input" id="user">
+                    <p>
+                        <i class="fa-regular fa-user"></i>
                     </p>
+                </div>
             </div>
-        </div>
-        <input type="submit" value="Ingresar">
+            <div class="form-content">
+                <label for="password">Contraseña</label>
+                <div class="form-input">
+                        <input type="password" name="password" class="input" id="password">
+                        <p class="hide-password hide">
+                        <i class="fa-regular fa-eye"></i>
+                        </p>
+                </div>
+            </div>
+            <input type="submit" value="Ingresar">
         </form>
-        <form class="form-register">
-        <h3>Registro</h3>
+        <form action="/register_user" method="post" class="form-register">
+            <h3>Registro</h3>
+            @csrf
             <div class="form-content">
                 <label for="nombre">Nombre</label>
                 <div class="form-input">
-                    <input type="text" id="nombre" name="Nombre" required>
+                    <input type="text" id="nombre" name="name" required>
                     <p>
                     <i class="fa-regular fa-user"></i>
                     </p>
@@ -47,7 +50,7 @@
             <div class="form-content">
                 <label for="correo">Correo electrónico:</label>
                 <div class="form-input">
-                    <input type="text" id="correo" required>
+                    <input type="text" id="correo" name="email" required>
                     <p>
                     <i class="fa-solid fa-at"></i>
                     </p>
